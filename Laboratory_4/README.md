ca08361e015246c8c61484f6d79edd0880da1144
kaijie <jayne.kaijie.fan@gmail.com>
2026-03-20 09:34:25 +0100

    Add files via upload

---
+++ Laboratory_4/README.md
@@ -0,0 +1,29 @@
+# Laboratory 3
+
+## Exercise 1
+
+Given a command-line parameter n, generate two random square matrices, perform a general matrix multiplication using OpenBLAS. 
+1)Benchmark its runtime by performing two warm-up cycles and averaging over 10 rounds. 
+2)Compute the average with both arithmetic and geometric mean.
+3)Complete the provided Makefile and use the provided sbatch script.
+
+You can find a template code inside "ex1/". Keep all the benchmark function into the library "ex1/src/my_time_lib.c" and "ex1/include/my_time_lib.h". 
+
+## Exercise 2
+Benchmark a program that use OpenBLAS to perform a general matrix multiplication between two random square matrices. Get the execution time and the FLOP/s.
+
+Implement your own general matrix multiplication and compare your performance with the OpenBLAS implementation.
+
+The template code is inside "ex2/"
+
+## Exercise 3
+Implement a simple GEMM algorithm that multiplies two matrices of size NxN by splitting the orginal matrix into blocks
+1)The block_size is such that  NxN is a multiple of block_size X block_size
+2)Measure the FLOPS of your sequential implementations
+Note: Perform a correctness check. And for debugging, compile the code using –g option;
+
+Run the code with valgrind to measure the cache hit/miss
+
+
+
+
---
+++ Laboratory_4/ex2/gemm.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <cblas.h>
+#include <string.h>
+
+#define dtype double
+#define NITER 10
+#define WARMUP 2
+
+#include "include/my_time_lib.h"
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("Usage: %s n \n\n"
+               "Where:\n"
+               "\tmatrix A has size n x n\n"
+               "\tmatrix B has size n x n\n"
+               "\tresult matrix C has size n x n\n",
+               argv[0]);
+        return 1;
+    }
+    dtype *A, *B, *C;
+    double timers[NITER];
+
+    /* EX1: Generate now two square matrices A and B of size (2^n) and fill them with random doubles.
+     *  After this, compute the matrix multiplication C = A x B using OpenBLAS: cblas_dgemm()
+     *
+     * Tasks:
+     *      1) Record CPU time on time[] vector. 
+     *      2) Compute their arithmetic mean and geometric mean.
+     *
+     * NOTE:
+     *      1) Fill A and B with random doubles
+     *      2) Init C with 0.0
+     */
+
+    /* |========================================| */
+    /* |           Put here your code           | */
+    /* |========================================| */
+
+
+
+    /*  EX1: Here we compute the vectors' arithmetic mean and geometric mean; 
+     *  these functions must be implemented inside
+     *   of the library "src/my_time_lib.c" (and their headers in "include/my_time_lib.h").
+     */
+    double a_mean = 0.0, g_mean = 0.0;
+
+    /* |========================================| */
+    /* |           Put here your code           | */
+    /* |========================================| */
+
+
+    printf(" %10s | %10s | %10s |\n", "v name", "arithmetic mean", "geometric mean");
+    printf(" %10s | %10f | %10f |\n", "time", a_mean, g_mean);
+
+    /*  EX2: Here we compute the FLOPS using arithmetic mean of time vector
+     *
+     */
+    double flops_openblas = 0.0;
+    /* |========================================| */
+    /* |           Put here your code           | */
+    /* |========================================| */
+
+    fprintf(stdout, "The OpenBLAS gemm achieved %lf MFLOP/s\n", flops_openblas / 1.e6);
+
+
+    /*  EX2: Implement your own general matrix multiplication
+     *  Compare your performance with the OpenBLAS one
+     *  Note: Perform a correctness check   
+     */
+    double flops_mygemm = 0.0;
+    /* |========================================| */
+    /* |           Put here your code           | */
+    /* |========================================| */
+    fprintf(stdout, "My gemm achieved %lf MFLOP/s\n", flops_mygemm / 1.e6);
+    return(0);
+}
